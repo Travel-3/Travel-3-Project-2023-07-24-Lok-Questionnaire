af7de73c721a5619f48fdd0dc2a884722fdf013e
Chonwai <chonwaiun@gmail.com>
2023-08-17 22:27:12 +0800

    Fix: Fixed the error when build.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,24 +1,26 @@
 // File Path:src/utils/utils.ts
 
+/**
+ * Get the platform type based on the user agent.
+ * @returns {string} The platform type: "iOS", "Android", "Desktop", or "unknown".
+ */
 function getPlatform() {
   // Check if we're running on the server or client
   if (typeof window === "undefined" || typeof navigator === "undefined") {
     return "unknown";
   }
 
-  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  const userAgent =
+    navigator.userAgent || navigator.vendor || (window as any).opera;
 
-  // iOS detection from: http://stackoverflow.com/a/9039885/177710
-  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+  if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
     return "iOS";
   }
 
-  // Android detection from: http://stackoverflow.com/a/13808003/177710
   if (/android/i.test(userAgent)) {
     return "Android";
   }
 
-  // Desktop detection
   if (/Windows|Macintosh|Linux|Ubuntu/.test(userAgent)) {
     return "Desktop";
   }
