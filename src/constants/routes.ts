09fe801794a0281ffbf75596c223c6be48bc8a7d
Roy <1997roylee@gmail.com>
2023-08-21 20:12:37 +0800

    update route

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,6 @@
 export const Routes: Record<string, string> = {
-  "#設計專業路線": "https://tally.so/r/meQxVE",
-  "#設計愛好者路線": "https://tally.so/r/nWEpLP",
+  "#設計專業路線": "https://tally.so/r/mZEGR0",
+  "#設計愛好者路線": "https://tally.so/r/3Nl4BG",
   "#設計與商業路線": "https://tally.so/r/woeVQP",
   "#設計週全路線": "https://tally.so/r/wA2bEW",
 };
