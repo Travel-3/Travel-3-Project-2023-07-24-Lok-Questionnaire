24b579a6c86cbd6f85c83be0a5255bd894d61e05
Chonwai <chonwaiun@gmail.com>
2023-08-22 00:16:39 +0800

    Update: Updated the result routes.

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,6 @@
 export const Routes: Record<string, string> = {
-  "#設計專業路線": "https://tally.so/r/mZEGR0",
-  "#設計愛好者路線": "https://tally.so/r/3Nl4BG",
-  "#設計與商業路線": "https://tally.so/r/woeVQP",
+  "#設計專業路線": "https://tally.so/r/wA2bEW",
+  "#設計愛好者路線": "https://tally.so/r/wA2bEW",
+  "#設計與商業路線": "https://tally.so/r/wA2bEW",
   "#設計週全路線": "https://tally.so/r/wA2bEW",
 };
