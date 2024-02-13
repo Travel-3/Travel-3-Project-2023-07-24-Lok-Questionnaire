70dbeda682bbb80e2c7a42685e769f1f53a5d297
0xRoy <1997roylee@gmail.com>
2024-02-13 15:07:28 +0800

    add hooks

---
+++ src/hooks/useUserScore.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+
+export default function useUserScore(game: string, sessionId: string) {
+  return useQuery({
+    queryKey: ["User", game, sessionId, "Score"],
+    queryFn: () => {
+      return fetch("/api/score?sessionId=" + sessionId + "&game=" + game).then(
+        (res) => res.json(),
+      );
+    },
+    enabled: !!sessionId,
+    refetchOnWindowFocus: true,
+    refetchIntervalInBackground: true,
+    refetchInterval: 10_000,
+  });
+}
